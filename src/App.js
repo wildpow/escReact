fbaaa93a517d81cf086e23156a9a25b1c3e1bff2
Aaron Wilder <element1028@gmail.com>
2018-01-09 16:25:53 -0800

    Fixed the logo popping add anination

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import Logo from './Components/LogoComponent/Logo.js';
+import './main.css';
 
 const Container = styled.div`
   margin-left: auto;
@@ -8,23 +10,23 @@ const Container = styled.div`
   padding-left: 15px;
   padding-right: 15px;
   
-  @media (min-width: 1200px) {
-    width: 1170px;
-  }
-
-  @media (min-width: 992px) {
-    width: 970px;
-  }
-
-  @media (min-width: 768px) {
-    width: 750px;
-  }
+  @media (min-width: 1200px) { width: 1170px; }
+  @media (min-width: 992px) { width: 970px; }
+  @media (min-width: 768px) { width: 750px; }
 `
 class App extends Component {
   render() {
     return (
       <Container>
-        <Logo />
+        <ReactCSSTransitionGroup
+          transitionName="fadeIn"
+          transitionAppear={true}
+          transitionAppearTimeout={500}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
+          <Logo />
+        </ReactCSSTransitionGroup>
       </Container>
     );
   }
--- src/Components/LogoComponent/Styles.js
+++ src/Components/LogoComponent/Styles.js
@@ -4,39 +4,34 @@ export const Header = styled.header`
   display: flex;
   flex-direction: column;
   font-family: 'Roboto', sans-serif;
-  margin-top: -13px;
+  margin-top: -18px;
 
   @media (min-width: 768px) {
-    margin-top: -22px;
+    margin-top: -35px;
   }
 `
 export const Image = styled.img`
   z-index: 1000;
   align-self: center;
-  max-height: 6.5em;
-
-  @media (min-width: 768px) {
-    max-height: 12em;
-  }
+  height: 6.5em;
+  @media (min-width: 500px) { height: 8em; }
+  @media (min-width: 768px) { height: 12em; }
 `
 export const H1 = styled.h1`
   align-self: center;
   margin-top: -26px;
-  padding: 10px;
-  text-shadow:0 0 1px transparent;
+  margin-bottom: 0;
   color: #c62828;
   font-size: 1.6rem;
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8);
-  margin-bottom: 12px;
   border: 2px solid black;
   box-shadow: 0 10px 6px -6px rgba(119, 119, 119, .9); 
   border-top-left-radius: .11rem;
   border-top-right-radius: .11rem;
+  padding: 5px 15px 5px 15px;
 
-  @media (min-width: 768px) {
-    font-size: 4rem;
-    margin-top: -47px;
-  }
+  @media (min-width: 500px) { font-size: 2.5rem; margin-top: -32px; }
+  @media (min-width: 768px) { font-size: 4rem; margin-top: -47px; }
 `
 export const Span = styled.span`
   color: #1565c0;
